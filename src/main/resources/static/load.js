04bf3745563686fc0c856e8c4eed394972f1e282
Vlad Mihajlneko <vladmyh@gmail.com>
2016-11-02 22:10:40 +0200

    Added message refresh

--- src/main/resources/static/load.js
+++ src/main/resources/static/load.js
@@ -1,11 +1,24 @@
 $(document).ready(function() {
-    $.ajax({
-        url: "http://localhost:8080/messages",
-        dataType: "json",
-        method: "GET",
-        headers: {"content-type": "application/json; charset=utf-8",
+
+    function appendMessage(elem, toAppend) {
+        elem.append("<div class=\"panel panel-default\"> <div class=\"panel-body\"><b>" +
+                            toAppend.author + "</b> : " + toAppend.message + "</div></div>");
+    }
+
+
+    setInterval(function() {
+        document.getElementById("messages").innerHTML = "";
+        $.ajax({
+            url: "http://localhost:8080/messages",
+            dataType: "json",
+            method: "GET",
+            headers: {"content-type": "application/json; charset=utf-8",
                 "Access-Control-Allow-Origin": "*"}
-    }).then(function(data) {
-        $('.content').append("<p>" + data + "</p>>");
-    });
+        }).then(function(data) {
+            data.forEach(function (d) {
+                appendMessage($('.content'), d);
+            })
+        });
+    }, 3000);
+
 });
